1f1536ca792312dcb7a76d29970c0109fdd14f4c
Aydin Seven <40699961+lordvader89a@users.noreply.github.com>
2020-12-10 18:03:57 +0100

    intial commit
    initial commit with all files for the database as well as the template I got

---
+++ data/data.sql
@@ -0,0 +1,94 @@
+INSERT INTO "Nutzer" ("Email", "Passwort", "Vorname", "Nachname")
+VALUES ('test@gmail.com', 'E1E1EEEEE', 'TestVorname', 'TestNachname'),
+       ('ist@test.de', 'E1E1EEEEE', 'Thomas', 'Tester'),
+       ('hhu@test.de', 'E1E1EEEEE', 'Heinrich', 'Heine'),
+       ("drei@hhu.de", "E1E1EEEEE", "drei", "Drei"),
+('vier@hhu.de', 'E1E1EEEEE', 'vier', 'Vier'),
+('vier1@hhu.de', 'E1E1EEEEE', 'vier', 'Vier'),
+('vier2@hhu.de', 'E1E1EEEEE', 'vier', 'Vier'),
+('fuenf@hhu.de', 'E1E1EEEEE', 'fünf', 'Fünf'),
+('sechs@hhu.de', 'E1E1EEEEE', 'sechs', 'Sechs');
+
+INSERT INTO "Adresse" ("id", "Stadt", "PLZ", "Strasse", "Hausnummer")
+VALUES (1, 'Düsseldorf', 40647, 'TestStrasse', '12'),
+       (2, 'Köln', 30647, 'StrasseKoeln', '12'),
+       (3, 'Frankfurt', 20647, 'StrasseFrankfurt', '12');
+
+INSERT INTO "Öffnungszeiten"("id","Tag", "Anfang", "Ende")
+VALUES (1, 0, '08:00:00', '20:00:00'),
+       (2, 1, '10:00:00', '08:00:00');
+
+INSERT INTO "Fahrzeugklasse"("Bezeichnung")
+VALUES('AM'),
+        ('B');
+
+INSERT INTO "Schüler"("Email","Geschlecht","Adresse")
+VALUES('test@gmail.com','m',1),
+('vier2@hhu.de','m',1),
+('vier1@hhu.de','m',1),
+("ist@test.de",'w',2);
+
+INSERT INTO "Admin"("Email", "Telefonnummer")
+VALUES("drei@hhu.de", '01111111111'),
+("vier@hhu.de",'02222222222');
+
+INSERT INTO "Fahrschule"("Email", "Website", "Bezeichnung", "Admin")
+VALUES('fahr1@hhu.de', 'https://eins', 'Erste', "drei@hhu.de"),
+('fahr2@hhu.de', 'https://zwei', 'Zweite',"vier@hhu.de");
+
+INSERT INTO "Prüfung"("id", "Typ", "Teilnahmegebühr")
+VALUES(0,0,50.36566),
+(1,0,30.55555);
+
+INSERT INTO "Führerschein"("Schüler", "Austellungsdatum", "Gültigkeitsdatum", "id")
+VALUES('test@gmail.com', date('now'), '2021-10-07', 0),
+("ist@test.de", date('now'), '2021-10-22', 1);
+
+INSERT INTO "Fahrlehrer"("Email", "Fahrlehrerlizenz")
+VALUES('hhu@test.de', date('now')),
+('vier@hhu.de', date('now')),
+('sechs@hhu.de', date('now')),
+('fuenf@hhu.de', date('now'));
+
+INSERT INTO "theoretische_Übung"("id", "Pflicht", "Dauer", "Thema", "Fahrschule")
+VALUES(0, 0, 80, 'Blick', 'fahr1@hhu.de'),
+(1, 0, 80, 'a', 'fahr1@hhu.de'),
+(2, 0, 80, 'b', 'fahr1@hhu.de'),
+(3, 0, 80, 'c', 'fahr1@hhu.de'),
+(4, 1, 60, 'Vorfahrt','fahr2@hhu.de');
+
+INSERT INTO "Fahrstunde"("Schüler", "Fahrlehrer", "Fahrschule", "Dauer", "Typ", "Preis", "id")
+VALUES('test@gmail.com','hhu@test.de','fahr1@hhu.de', 180, 'Autobahn', 120, 0),
+("ist@test.de", 'fuenf@hhu.de', 'fahr2@hhu.de', 180,'Autobahn', 120, 1),
+("vier1@hhu.de", 'sechs@hhu.de', 'fahr2@hhu.de', 180,'Autobahn', 120, 2),
+("vier2@hhu.de", 'sechs@hhu.de', 'fahr2@hhu.de', 180,'Autobahn', 120, 3),
+("vier2@hhu.de", 'fuenf@hhu.de', 'fahr2@hhu.de', 180,'Autobahn', 120, 4);
+
+INSERT INTO "Fahrzeug"("Fahrzeugklasse", "Fahrschule", "Kennzeichen", "HU-Eintrag", "Erstzulassung")
+VALUES('AM', 'fahr1@hhu.de', 'ME1', '2021-10-07','2013-10-07'),
+('B', 'fahr2@hhu.de', 'ME2', '2021-10-22','2013-10-22');
+
+INSERT INTO "Fahrschule_hat_Öffnungszeiten"("Fahrschule", "Öffnungszeiten", "id")
+VALUES('fahr1@hhu.de', 1, 0),
+('fahr2@hhu.de', 2, 1);
+
+INSERT INTO "Führerschein_Erlaubt_Fahrzeugklasse"("Führerschein", "Fahrzeugklasse", "id")
+VALUES(0, 'AM',0),
+(1,'B',1);
+
+INSERT INTO "Fahrschule_besitzt_Adresse"("Fahrschule", "Adresse", "id")
+VALUES('fahr1@hhu.de', 1, 0),
+('fahr2@hhu.de', 2, 1);
+
+INSERT INTO "Schüler_Belegt_Theoretische_Übung"("Schüler", "theoretische_Übung", "id")
+VALUES('test@gmail.com', 0, 0),
+('test@gmail.com', 1, 5),
+('test@gmail.com', 2, 6),
+("ist@test.de", 0, 1),
+("ist@test.de", 1, 2),
+("ist@test.de", 2, 3),
+("ist@test.de", 3, 4);
+
+INSERT INTO "Schüler_Belegt_Prüfung"("Schüler", "Prüfung", "Erfolgreich", "id")
+VALUES('test@gmail.com', 1, 0, 0),
+("ist@test.de", 0, 1, 1);
---
+++ data/schema.sql
@@ -0,0 +1,240 @@
+PRAGMA auto_vacuum = 1;
+PRAGMA encoding = "UTF-8";
+PRAGMA foreign_keys = 1;
+PRAGMA journal_mode = WAL;
+PRAGMA synchronous = NORMAL;
+
+DROP TABLE IF EXISTS "Adresse";
+DROP TABLE IF EXISTS "Nutzer";
+DROP TABLE IF EXISTS "Öffnungszeiten";
+DROP TABLE IF EXISTS "Fahrzeugklasse";
+DROP TABLE IF EXISTS "Schüler";
+DROP TABLE IF EXISTS "Admin";
+DROP TABLE IF EXISTS "Fahrschule";
+DROP TABLE IF EXISTS "Prüfung";
+DROP TABLE IF EXISTS "Führerschein";
+DROP TABLE IF EXISTS "Fahrlehrer";
+DROP TABLE IF EXISTS "theoretische_Übung";
+DROP TABLE IF EXISTS "Fahrstunde";
+DROP TABLE IF EXISTS "Fahrzeug";
+DROP TABLE IF EXISTS "Fahrschule_hat_Öffnungszeiten";
+DROP TABLE IF EXISTS "Führerschein_Erlaubt_Fahrzeugklasse";
+DROP TABLE IF EXISTS "Fahrschule_besitzt_Adresse";
+DROP TABLE IF EXISTS "Schüler_Belegt_Theoretische_Übung";
+DROP TABLE IF EXISTS "Schüler_Belegt_Prüfung";
+
+
+CREATE TABLE IF NOT EXISTS "Adresse" (
+	"id"	INTEGER UNIQUE,
+	"Stadt"	varchar(30) NOT NULL CHECK(trim("Stadt") NOT LIKE '' AND "Stadt" NOT GLOB '*[-~]*' AND "Stadt" NOT GLOB '*[0-9]*'),
+	"PLZ"	INTEGER NOT NULL CHECK("PLZ" BETWEEN 10000 AND 99999),
+	"Strasse"	varchar(30) NOT NULL CHECK(trim("Strasse") NOT LIKE '' AND "Strasse" NOT GLOB '*[-~]*' AND "Strasse" NOT GLOB '*[0-9]*'),
+	"Hausnummer"	varchar(10) NOT NULL CHECK(trim("Hausnummer") NOT LIKE '' AND "Hausnummer" NOT GLOB '*[-~]*'),
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+
+CREATE TABLE IF NOT EXISTS "Nutzer" (
+	"Email"	varchar(30) NOT NULL CHECK(ltrim(lower("Email"), 'qwertzuiopüasdfghjklöäyxcvbnm1234567890') LIKE '@%.%' AND ltrim(lower("Email"), '@qwertzuiopüasdfghjklöäyxcvbnm1234567890') LIKE '.%' AND rtrim(lower("Email"), 'qwertzuiopüasdfghjklöäyxcvbnm') LIKE '%@%.' AND "Email" NOT LIKE '@%.%' AND "Email" NOT LIKE '%@.%' AND "Email" NOT LIKE '%@%.') COLLATE NOCASE UNIQUE,
+	"Passwort"	varchar(50) NOT NULL CHECK(length("Passwort") >= 5 AND "Passwort" NOT GLOB '*[0-9][0-9]*' AND "Passwort" GLOB '*[A-Z]*' AND "Passwort" GLOB '*[0-9]*[0-9]*'),
+	"Vorname"	varchar(30) NOT NULL CHECK(trim("Vorname") NOT LIKE '' AND "Vorname" NOT GLOB '*[-~]*'),
+	"Nachname"	varchar(30) NOT NULL CHECK(trim("Nachname") NOT LIKE '' AND "Nachname" NOT GLOB '*[-~]*'),
+	PRIMARY KEY("Email")
+);
+
+CREATE TABLE IF NOT EXISTS "Öffnungszeiten" (
+	"id"	INTEGER UNIQUE,
+	"Tag"	INTEGER NOT NULL CHECK("Tag" BETWEEN 0 AND 6) UNIQUE,
+	"Anfang"	varchar(8) NOT NULL CHECK(trim("Anfang") NOT LIKE '' AND "Anfang" IS time("Anfang")),
+	"Ende"	varchar(8) NOT NULL CHECK("Ende" IS time("Ende")),
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+
+CREATE TABLE IF NOT EXISTS "Fahrzeugklasse" (
+	"Bezeichnung"	varchar(30) NOT NULL CHECK(trim("Bezeichnung") NOT LIKE '' AND "Bezeichnung" NOT GLOB '*[-~]*' AND "Bezeichnung" NOT GLOB '*[0-9]*') UNIQUE,
+	PRIMARY KEY("Bezeichnung")
+);
+
+CREATE TABLE IF NOT EXISTS "Prüfung" (
+	"id"	INTEGER UNIQUE,
+	"Typ"	INTEGER NOT NULL CHECK("Typ" =0 OR "Typ" = 1),
+	"Teilnahmegebühr"	Float NOT NULL CHECK("Teilnahmegebühr" > 0 AND round("Teilnahmegebühr",2)),
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+
+CREATE TABLE IF NOT EXISTS "Admin" (
+	"Email"	varchar(30) NOT NULL UNIQUE,
+	"Telefonnummer"	varchar(20) NOT NULL CHECK("Telefonnummer" LIKE '0%' AND substr("Telefonnummer", 1) GLOB '*[0-9]*') UNIQUE,
+	PRIMARY KEY("Email"),
+	FOREIGN KEY("Email") REFERENCES "Nutzer"("Email") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS "Fahrschule" (
+	"Email"	varchar(30) NOT NULL CHECK(ltrim(lower("Email"), 'qwertzuiopüasdfghjklöäyxcvbnm1234567890') LIKE '@%.%' AND ltrim(lower("Email"), '@qwertzuiopüasdfghjklöäyxcvbnm1234567890') LIKE '.%' AND rtrim(lower("Email"), 'qwertzuiopüasdfghjklöäyxcvbnm') LIKE '%@%.' AND "Email" NOT LIKE '@%.%' AND "Email" NOT LIKE '%@.%' AND "Email" NOT LIKE '%@%.') COLLATE NOCASE UNIQUE,
+	"Website"	varchar(30) NOT NULL CHECK(trim("Website") NOT LIKE '' AND "Website" NOT GLOB '*[-~]' AND "Website" LIKE 'https://%') COLLATE NOCASE,
+	"Bezeichnung"	varchar(30) NOT NULL CHECK(trim("Bezeichnung") NOT LIKE '' AND "Bezeichnung" NOT GLOB '*[-~]*' AND "Bezeichnung" NOT GLOB '*[0-9]*'),
+	"Admin"	varchar(30) NOT NULL,
+	PRIMARY KEY("Email"),
+	FOREIGN KEY("Admin") REFERENCES "Admin"("Email") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS "Schüler" (
+	"Email"	varchar(30) NOT NULL UNIQUE,
+	"Geschlecht"	varchar(1) CHECK("Geschlecht" LIKE "m" OR "Geschlecht" LIKE "w" OR "Geschlecht" LIKE "d"),
+	"Adresse"	INTEGER NOT NULL,
+	PRIMARY KEY("Email"),
+	FOREIGN KEY("Email") REFERENCES "Nutzer"("Email") ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY("Adresse") REFERENCES "Adresse"("id") ON DELETE cascade ON UPDATE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS "Führerschein" (
+	"Schüler"	varchar(30) NOT NULL,
+	"Austellungsdatum"	varchar(20) NOT NULL CHECK(trim("Austellungsdatum") NOT LIKE '' AND "Austellungsdatum" IS date("Austellungsdatum")),
+	"Gültigkeitsdatum"	varchar(20) NOT NULL CHECK(trim("Gültigskeitsdatum") NOT LIKE '' AND "Gültigkeitsdatum" IS date("Gültigkeitsdatum")),
+	"id"	INTEGER UNIQUE,
+	PRIMARY KEY("id"),
+	FOREIGN KEY("Schüler") REFERENCES "Schüler"("Email") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS "Fahrlehrer" (
+	"Email"	varchar(30) NOT NULL UNIQUE,
+	"Fahrlehrerlizenz"	varchar(20) NOT NULL CHECK(trim("Fahrlehrerlizenz") NOT LIKE '' AND "Fahrlehrerlizenz" IS date("Fahrlehrerlizenz")),
+	PRIMARY KEY("Email"),
+	FOREIGN KEY("Email") REFERENCES "Nutzer"("Email")
+);
+
+CREATE TABLE IF NOT EXISTS "theoretische_Übung" (
+	"id"	INTEGER UNIQUE,
+	"Pflicht"	INTEGER NOT NULL CHECK("Pflicht" = 1 OR "Pflicht" = 0),
+	"Dauer"	INTEGER NOT NULL CHECK("Dauer" > 0),
+	"Thema"	varchar(30) NOT NULL CHECK(trim("Thema") NOT LIKE '' AND "Thema" GLOB '*[a-zA-Z]*'),
+	"Fahrschule"	varchar(30) NOT NULL,
+	PRIMARY KEY("id" AUTOINCREMENT),
+	FOREIGN KEY("Fahrschule") REFERENCES "Fahrschule"("Email") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS "Fahrstunde" (
+	"Schüler"	INTEGER NOT NULL,
+	"Fahrlehrer"	INTEGER NOT NULL,
+	"Fahrschule"	INTEGER NOT NULL,
+	"Dauer"	INTEGER NOT NULL CHECK("Dauer" > 0 AND "Dauer" % 45 = 0),
+	"Typ"	INTEGER NOT NULL CHECK(trim("Typ") NOT LIKE '' AND "Typ" GLOB '*[^-~]*' AND "Typ" NOT GLOB '*[0-9]*'),
+	"Preis"	Float NOT NULL CHECK("Preis" > 0),
+	"id"	INTEGER UNIQUE,
+	PRIMARY KEY("id" AUTOINCREMENT),
+	FOREIGN KEY("Fahrschule") REFERENCES "Fahrschule"("Email") ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY("Fahrlehrer") REFERENCES "Fahrlehrer"("Email") ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY("Schüler") REFERENCES "Schüler"("Email") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS "Fahrzeug" (
+	"Fahrzeugklasse"	varchar(30) NOT NULL,
+	"Fahrschule"	varchar(30) NOT NULL,
+	"Kennzeichen"	varchar(12) NOT NULL CHECK(trim("Kennzeichen") NOT LIKE '') UNIQUE,
+	"HU-Eintrag"	varchar(30) NOT NULL CHECK(trim("HU-Eintrag") NOT LIKE '' AND "HU-Eintrag" IS date("HU-Eintrag")),
+	"Erstzulassung"	varchar(30) NOT NULL CHECK(trim("Erstzulassung") NOT LIKE '' AND "Erstzulassung" IS date("Erstzulassung")),
+	PRIMARY KEY("Kennzeichen"),
+	FOREIGN KEY("Fahrschule") REFERENCES "Fahrschule"("Email") ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY("Fahrzeugklasse") REFERENCES "Fahrzeugklasse"("Bezeichnung") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS "Führerschein_Erlaubt_Fahrzeugklasse" (
+	"Führerschein"	INTEGER NOT NULL,
+	"Fahrzeugklasse"	varchar(30) NOT NULL,
+	"id"	INTEGER UNIQUE,
+	FOREIGN KEY("Führerschein") REFERENCES "Führerschein"("id") ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY("Fahrzeugklasse") REFERENCES "Fahrzeugklasse"("Bezeichnung") ON DELETE CASCADE ON UPDATE CASCADE,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+
+CREATE TABLE IF NOT EXISTS "Fahrschule_besitzt_Adresse" (
+	"Fahrschule"	varchar(30) NOT NULL,
+	"Adresse"	INTEGER NOT NULL,
+	"id"	INTEGER UNIQUE,
+	FOREIGN KEY("Adresse") REFERENCES "Adresse"("id") ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY("Fahrschule") REFERENCES "Fahrschule"("Email") ON DELETE CASCADE ON UPDATE CASCADE,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+
+CREATE TABLE IF NOT EXISTS "Fahrschule_hat_Öffnungszeiten" (
+	"Fahrschule"	varchar(30) NOT NULL,
+	"Öffnungszeiten"	INTEGER NOT NULL,
+	"id"	INTEGER UNIQUE,
+	FOREIGN KEY("Öffnungszeiten") REFERENCES "Öffnungszeiten"("id"),
+	FOREIGN KEY("Fahrschule") REFERENCES "Fahrschule"("Email") ON DELETE CASCADE ON UPDATE CASCADE,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+
+CREATE TABLE IF NOT EXISTS "Schüler_Belegt_Prüfung" (
+	"Schüler"	INTEGER NOT NULL,
+	"Prüfung"	INTEGER NOT NULL,
+	"Erfolgreich"	INTEGER NOT NULL CHECK("Erfolgreich" = 1 OR "Erfolgreich" = 0),
+	"id"	INTEGER UNIQUE,
+	FOREIGN KEY("Schüler") REFERENCES "Schüler"("Email") ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY("Prüfung") REFERENCES "Prüfung"("id") ON DELETE CASCADE ON UPDATE CASCADE,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+
+CREATE TABLE IF NOT EXISTS "Schüler_Belegt_Theoretische_Übung" (
+	"Schüler"	varchar(30) NOT NULL,
+	"theoretische_Übung"	INTEGER NOT NULL,
+	"id"	INTEGER UNIQUE,
+	FOREIGN KEY("theoretische_Übung") REFERENCES "theoretische_Übung"("id") ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY("Schüler") REFERENCES "Schüler"("Email") ON DELETE CASCADE ON UPDATE CASCADE,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+
+
+DROP TRIGGER IF EXISTS "checkFahrstunden";
+CREATE TRIGGER checkFahrstunden BEFORE INSERT ON Schüler_Belegt_Prüfung
+BEGIN
+SELECT RAISE(ABORT, "Der Schüler hat noch nicht genug Fahrstunden absolviert!")
+WHERE EXISTS (SELECT Typ FROM Prüfung
+	WHERE Typ = 1 AND (SELECT sum(Fahrstunde.Dauer)
+        FROM Fahrstunde, Schüler_Belegt_Prüfung
+        WHERE Fahrstunde.Schüler = Schüler_Belegt_Prüfung.Schüler
+        	AND Prüfung.id=Schüler_Belegt_Prüfung.Prüfung)<180);
+END;
+/*Trigger checkTheorie funktioniert nicht, er löst immer aus 
+Theorie-Typ=0
+Praxis-Typ=1
+Pflicht=TRUE(0) OR FALSE(1)*/
+
+/*DROP TRIGGER IF EXISTS "checkTheorie";
+CREATE TRIGGER checkTheorie
+BEFORE INSERT ON Schüler_Belegt_Prüfung FOR EACH ROW
+BEGIN
+SELECT RAISE(ABORT,"Der Schüler hat noch nicht genug Pflicht-Übungen besucht!") 
+	WHERE EXISTS (SELECT Typ
+		FROM Prüfung
+		WHERE Typ = 0 AND (SELECT count(DISTINCT theoretische_Übung.Thema)
+			FROM Schüler_Belegt_Prüfung, Schüler_Belegt_Theoretische_Übung, theoretische_Übung
+			WHERE Schüler_Belegt_Theoretische_Übung.Schüler = Schüler_Belegt_Prüfung.Schüler
+				AND Prüfung.id = Schüler_Belegt_Prüfung.Prüfung
+				AND theoretische_Übung.id = Schüler_Belegt_Theoretische_Übung.theoretische_Übung
+				AND theoretische_Übung.Pflicht = 0)<3);
+END;*/
+
+/*Dieser Trigger löst nie aus, ich weiß nur nicht, warum.*/
+
+DROP TRIGGER IF EXISTS "openingTimes";
+CREATE TRIGGER openingTimes
+BEFORE INSERT ON Öffnungszeiten FOR EACH ROW
+BEGIN
+SELECT RAISE(ABORT, 'Die Öffnungszeit liegt nach der Schließung!') WHERE EXISTS (SELECT id
+	FROM Öffnungszeiten
+	WHERE Anfang > Ende);
+END;
+
+DROP TRIGGER IF EXISTS "roundingLesson";
+CREATE TRIGGER roundingLesson
+AFTER INSERT ON Fahrstunde FOR EACH ROW
+BEGIN
+	UPDATE Fahrstunde SET 'Preis' = round(Preis, 2);
+END;
+
+DROP TRIGGER IF EXISTS "roundingExam";
+CREATE TRIGGER roundingExam
+AFTER INSERT ON Prüfung FOR EACH ROW
+BEGIN
+	UPDATE Prüfung SET 'Teilnahmegebühr' = round(Teilnahmegebühr, 2);
+END;
